ba3c50d42eb159de4c2e9740352efc94640c6619
16617B <robert.oconnor@nazarethisd.net>
2022-10-14 14:50:30 -0500

    16617 b solo driver file added

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriverControlledSolo.java
@@ -0,0 +1,120 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.Range;
+
+@TeleOp (name = "Driver Solo", group = "Robot")
+
+public class DriverControlledSolo extends LinearOpMode {
+
+    HardwareRobot robot = new HardwareRobot();
+
+@Override
+public void runOpMode() {
+
+    robot.init(hardwareMap);
+    
+    telemetry.addData("Say", "Waiting for Start");
+    telemetry.update();
+    
+    waitForStart();
+    
+    while (opModeIsActive()){
+        
+    double Turn = gamepad1.left_stick_x;  //speed
+    double Speed = -gamepad1.left_stick_y; //turn
+    double Strafe = -gamepad1.right_stick_x;
+    double MAX_SPEED = 1.0;
+    
+    Turn = Turn*.9;
+    
+    double front_left = Speed + Turn - Strafe;
+    double front_right = Speed - Turn + Strafe;
+    double rear_left = Speed + Turn + Strafe;
+    double rear_right = Speed - Turn - Strafe;
+    
+    front_left = Range.clip(front_left, -1, 1);
+    front_right = Range.clip(front_right, -1, 1);
+    rear_left = Range.clip(rear_left, -1, 1);
+    rear_right = Range.clip(rear_right, -1, 1);
+    
+    
+    front_left = (float)scaleInput(front_left);
+    front_right = (float)scaleInput(front_right);
+    rear_left = (float)scaleInput(rear_left);
+    rear_right = (float)scaleInput(rear_right);
+    
+    front_left /=2;
+    front_right /=2;
+    rear_left /=2;
+    rear_right /=2;
+  /*
+    if (gamepad1.right_bumper){
+    front_left *=2;
+    front_right *=2;
+    rear_left *=2;
+    rear_right *=2;
+    }
+
+    if (gamepad1.left_bumper){
+    front_left /=2.5;
+    front_right /=2.5;
+    rear_left /=2.5;
+    rear_right /=2.5;
+    }
+   */
+   
+
+double grabberPosition = 0;
+   
+   if (gamepad1.a){
+    grabberPosition = .3;
+    }
+
+   double liftPower = 0;
+ if (gamepad1.left_bumper){
+    liftPower = 1;
+    }
+    
+    if (gamepad1.right_bumper){
+    liftPower = -1;
+    }
+
+    robot.leftFrontDrive.setPower(front_left);
+    robot.rightFrontDrive.setPower(front_right);
+    robot.leftRearDrive.setPower(rear_left);
+    robot.rightRearDrive.setPower(rear_right);
+    robot.lift.setPower(liftPower);
+    robot.grabber.setPosition(grabberPosition);
+
+    
+}
+
+}
+
+double scaleInput(double dVal)  {
+      double[] scaleArray = { 0.0, 0.05, 0.09, 0.10, 0.12, 0.15, 0.18, 0.24,
+            0.30, 0.36, 0.43, 0.50, 0.60, 0.72, 0.85, 1.00, 1.00 };
+      
+      // get the corresponding index for the scaleInput array.
+      int index = (int) (dVal * 16.0);
+      if (index < 0) {
+         index = -index;
+      } else if (index > 16) {
+         index = 16;
+      }
+      
+      double dScale = 0.0;
+      if (dVal < 0) {
+         dScale = -scaleArray[index];
+      } else {
+         dScale = scaleArray[index];
+      }
+      
+      return dScale;
+   }
+    
+}
+
+
